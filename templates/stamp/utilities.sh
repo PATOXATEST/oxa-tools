20f02f179b033e60f8e700886861313e9baa9091
John Patrick Papares <johnpatrickpapares@gmail.com>
2020-05-17 20:19:59 +0800

    Update

--- templates/stamp/utilities.sh
+++ templates/stamp/utilities.sh
@@ -212,7 +212,7 @@ install-mongodb-shell()
             wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
             echo "deb ${PACKAGE_URL} "${SHORT_CODENAME}"/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
         else
-            apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv B41E5E3969464050 #7F0CEB10
+            apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
             echo "deb ${PACKAGE_URL} "${SHORT_CODENAME}"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
         fi
 
